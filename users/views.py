731a7179fda1eefa9e0bcddbc7547d0c0e95d352
Abdullah Mahmood <abdu77ah.m@gmail.com>
2023-04-27 13:09:34 +0500

    Latest Version

--- users/views.py
+++ users/views.py
@@ -9,28 +9,11 @@ from django.contrib.auth.decorators import login_required
 from .forms import RegisterForm, LoginForm, UpdateUserForm, UpdateProfileForm
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
+import librosa
+import numpy as np
 from keras.models import load_model
-from keras.preprocessing import image
-import tensorflow as tf
-import json
-from tensorflow import Graph
 
-
-img_height, img_width=224,224
-with open('./models/imagenet_classes.json','r') as f:
-    labelInfo=f.read()
-
-labelInfo=json.loads(labelInfo)
-
-
-model_graph = Graph()
-with model_graph.as_default():
-    tf_session = tf.compat.v1.Session()
-    with tf_session.as_default():
-        model=load_model('./models/MobileNetModelImagenet.h5')
-
-
-
+model = load_model('./models/best_model.h5')
 
 def home(request):
     return render(request, 'users/home1.html')
@@ -45,19 +28,15 @@ def predictEmotion(request):
     fs=FileSystemStorage()
     filePathName=fs.save(fileObj.name,fileObj)
     filePathName=fs.url(filePathName)
-    testimage='.'+filePathName
-    img = image.load_img(testimage, target_size=(img_height, img_width))
-    x = image.img_to_array(img)
-    x=x/255
-    x=x.reshape(1,img_height, img_width,3)
-    with model_graph.as_default():
-        with tf_session.as_default():
-            predi=model.predict(x)
-
-    import numpy as np
-    predictedLabel=labelInfo[str(np.argmax(predi[0]))]
-
-    context={'filePathName':filePathName,'predictedLabel':predictedLabel[1]}
+    y, sr = librosa.load(filePathName, duration=3, offset=0.5)
+    mfcc = np.mean(librosa.feature.mfcc(y=y, sr=sr, n_mfcc=40).T, axis=0)
+    X_test = np.expand_dims(mfcc, axis=0)
+    X_test = np.expand_dims(X_test, axis=2)
+    # make a prediction
+    predictions = model.predict(X_test)
+    emotion_labels = ['neutral', 'happiness', 'sadness', 'surprise', 'anger', 'fearful', 'disgust', 'boredom']
+    predicted_label = emotion_labels[np.argmax(predictions)]
+    context={'filePathName':filePathName,'predictedLabel':predicted_label}
     return render(request,'users/home.html',context)
 
 
